4a8792fe93742dbf5a58649c1ac7dae2b6c679e3
Gerrit Eichner <gcb7@uni-giessen.de>
2017-09-28 17:05:18 +0200

    Added examples to kernels.R

--- R/kernels.R
+++ R/kernels.R
@@ -4,7 +4,7 @@
 ### Kernel functions for "Kernel adjusted density estimation" of Srihera &
 ### Stute (2011) and for "Rank Transformations in Kernel Density Estimation"
 ### of Eichner & Stute (2013)
-### R 3.3.2, 13./14.2.2017 (21./24./26./27./28./31.10./4./9.11./5.12.2016)
+### R 3.4.1, 13./14.2./28.9.2017 (21./24./26./27./28./31.10./4./9.11./5.12.2016)
 ###*****************************************************************************
 
 #' Epanechnikov kernel
@@ -14,6 +14,13 @@
 #' @param x Numeric vector.
 #' @return A numeric vector of the Epanechnikov kernel evaluated at the
 #'         values in \code{x}.
+#'
+#' @examples
+#' kader:::epanechnikov(x = c(-sqrt(6:5), -2:2, sqrt(5:6)))
+#' \donttest{
+#' curve(kader:::epanechnikov(x), from = -sqrt(6), to = sqrt(6))
+#' }
+#'
 epanechnikov <- function(x) {
   (abs(x) < sqrt(5)) * 3 / (4 * sqrt(5)) * (1 - x*x / 5)
 }
@@ -28,6 +35,12 @@ epanechnikov <- function(x) {
 #' @param b Numeric scalar: upper bound of kernel support; defaults to 0.5.
 #' @return A numeric vector of the rectangular kernel evaluated at the
 #'         values in \code{x}.
+#' @examples
+#' kader:::rectangular(x = seq(-1, 1, by = 0.1))
+#' \donttest{
+#' curve(kader:::rectangular(x), from = -1, to = 1)
+#' }
+#'
 rectangular <- function(x, a = -0.5, b = 0.5) {
   (a < x & x < b) / (b - a)
 }
